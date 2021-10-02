9171d4c6316cc78ba440d583198455ac8fc381ba
thomas434 <tomas.temo77@gmail.com>
2021-10-02 22:24:43 +0200

    add dwonload channel videos api

--- app/apis.py
+++ app/apis.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter, HTTPException
-from .schemas import SingleVideoSchema, PlaylistVideoSchema
-from .services import youtube_download_single_video, youtube_download_playlist
+from .schemas import SingleVideoSchema, PlaylistVideoSchema, ChannelVideoSchema
+from .services import (
+    youtube_download_single_video,
+    youtube_download_playlist,
+    youtube_download_channel_videos,
+)
 
 
 router = APIRouter()
@@ -15,4 +19,10 @@ async def download_single_video(data: SingleVideoSchema):
 @router.post("/playlist/")
 async def download_playlist_videos(data: PlaylistVideoSchema):
     youtube_download_playlist(data.playlist_url, data.save_path, data.quality)
+    return {"message": f"you will find your videos in {data.save_path}"}
+
+
+@router.post("/channel/")
+async def download_channel_videos(data: ChannelVideoSchema):
+    youtube_download_channel_videos(data.channel_url, data.save_path, data.quality)
     return {"message": f"you will find your videos in {data.save_path}"}
