636351abe0a1742855cccfbd997ea3bb1aa98a78
thomas434 <tomas.temo77@gmail.com>
2021-10-02 22:24:31 +0200

    add download channel video

--- app/services.py
+++ app/services.py
@@ -2,7 +2,7 @@ import logging
 from fastapi import HTTPException
 
 try:
-    from pytube import Playlist, YouTube
+    from pytube import Playlist, YouTube, Channel
     from pytube.exceptions import VideoUnavailable
 except ImportError:
     raise ImportError(
@@ -36,7 +36,7 @@ def youtube_download_single_video(url, output_path, quality="360p"):
     try:
         yt = YouTube(url)
     except VideoUnavailable:
-        raise HTTPException("Video URL invalid.")
+        raise HTTPException(status_code=400, detail="Video URL invalid.")
     else:
         stream = yt.streams.filter(res=quality, progressive=progressive(quality))
         stream.first().download(output_path=output_path)
@@ -60,7 +60,29 @@ def youtube_download_playlist(playlist_url, output_path, quality="360p"):
             stream.first().download(output_path=output_path)
 
     if failed_videos_url:
-        failed_urls = ", ".join(failed_videos_url)
+        failed_urls = "\n ".join(failed_videos_url)
+        return f"Failed Videos URLs are: {failed_urls}"
+
+    return True
+
+
+def youtube_download_channel_videos(channel_url, output_path, quality="360p"):
+    check_quality(quality=quality)
+
+    channel = Channel(channel_url)
+    failed_videos_url = []
+
+    for url in channel.video_urls:
+        try:
+            yt = YouTube(url)
+        except VideoUnavailable:
+            failed_videos_url.append(url)
+        else:
+            stream = yt.streams.filter(res=quality, progressive=progressive(quality))
+            stream.first().download(output_path=output_path)
+
+    if failed_videos_url:
+        failed_urls = "\n ".join(failed_videos_url)
         return f"Failed Videos URLs are: {failed_urls}"
 
     return True
